84432b74596bc97440e48b18783c51c931a0b2a0
Gokul Ram <ggokulaparna@gmail.com>
2026-01-01 03:53:01 +0530

    UI update

--- src/pages/mindScape.jsx
+++ src/pages/mindScape.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./mindscape.css";
+import "./mindScape.css";
 
 const Mindscape = () => {
   const [open, setOpen] = useState(null);
